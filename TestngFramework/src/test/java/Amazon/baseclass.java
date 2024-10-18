6c87730dee9eded026c53f2d208fc9176c549857
Harishk432 <harishgopinath876@gmail.com>
2024-10-18 19:34:55 +0530

    Update baseclass.java
    Deleted HI

--- TestngFramework/src/test/java/Amazon/baseclass.java
+++ TestngFramework/src/test/java/Amazon/baseclass.java
@@ -28,7 +28,7 @@ WebDriver driver;
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));//implicit
 		Screenshots("Openchrome");
-		//HI
+		
 		
 	}     
 	
